9081f677434e45ed835af49f1540c95e98f97d95
Prince <122357150+itzprincephutela@users.noreply.github.com>
2023-01-22 19:09:36 +0530

    Add files via upload

---
+++ 3RD.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int age = 22;
+    printf("age is %d\n" , age);
+    float pi = 3.14;
+    printf("pi is %f\n" ,pi);
+    char star = "*";
+    printf("star is %c",star);
+    return 0;
+
+}  
